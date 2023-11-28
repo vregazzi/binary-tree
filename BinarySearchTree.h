3c36b4a3766db3936ef6563afce4f5c95e79caf8
Vincent Regazzi <vincentregazzi@gmail.com>
2023-11-27 19:25:28 -0600

    add helper methods and new attributes

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -7,22 +7,19 @@ template <typename Comparable>
 class BinarySearchTree
 {
 private:
-    // make root
     BinarySearchTreeNode<Comparable> *root;
 
 public:
     BinarySearchTree() : root(nullptr) {}
 
+    /**
+     * @brief insert an item into a tree
+     *
+     * @param x the value to insert in the tree
+     */
     void insert(const Comparable &x)
     {
-        if (root == nullptr)
-        {
-            root = new BinarySearchTreeNode(x);
-        }
-        else
-        {
-            root->insert(x);
-        }
+        root = insert_node(root, x);
     }
 
     void inorder_print()
@@ -61,6 +58,29 @@ public:
         else
         {
             root->postorder_print();
+        }
+    }
+
+    /**
+     * @brief print the tree using "depthorder" traversal
+     */
+    void depthorder_print()
+    {
+        if (root == nullptr)
+        {
+            std::cout << "depth_print::Empty Binary Tree" << std::endl;
+        }
+        else
+        {
+            // the possible depth values for the tree run from
+            // 0 for the root of the tree to the height of the tree
+            int tree_height = root->height();
+            std::cout << "Tree Height: " << tree_height << std::endl;
+            for (int i = 0; i <= tree_height; ++i)
+            {
+                // for each possible depth, print just the nodes at that depth
+                root->print_depth(0, i);
+            }
         }
     }
 };
--- BinarySearchTreeNode.h
+++ BinarySearchTreeNode.h
@@ -3,81 +3,259 @@ template <typename Comparable>
 class BinarySearchTreeNode
 {
 public:
-    BinarySearchTreeNode(Comparable value) : value(value), left(nullptr), right(nullptr) {}
-    Comparable value;
-    BinarySearchTreeNode *left;
-    BinarySearchTreeNode *right;
-
-    void insert(const Comparable &x)
-    {
-        if (x < value)
-        {
-            if (left == nullptr)
-            {
-                left = new BinarySearchTreeNode(x);
-                return;
-            }
-            else
-            {
-                left->insert(x);
-                return;
-            }
-        }
-        else if (x > value)
-        {
-            if (right == nullptr)
-            {
-                right = new BinarySearchTreeNode(x);
-                return;
-            }
-            else
-            {
-                right->insert(x);
-                return;
-            }
-        }
-        else
-        {
-            throw std::logic_error("Duplicate value in tree");
-        }
-    }
+    BinarySearchTreeNode(
+        Comparable value,
+        BinarySearchTreeNode *left,
+        BinarySearchTreeNode *right)
+        : m_value(value), m_left(left), m_right(right) {}
+    Comparable m_value;
+    BinarySearchTreeNode *m_left;
+    BinarySearchTreeNode *m_right;
+    int m_height = 0;
 
     void preorder_print()
     {
-        std::cout << value << std::endl;
-        if (left != nullptr)
+        std::cout << m_value << std::endl;
+        if (m_left != nullptr)
         {
-            left->preorder_print();
+            m_left->preorder_print();
         }
-        if (right != nullptr)
+        if (m_right != nullptr)
         {
-            right->preorder_print();
+            m_right->preorder_print();
         }
     }
 
     void postorder_print()
     {
-        if (left != nullptr)
+        if (m_left != nullptr)
         {
-            left->postorder_print();
+            m_left->postorder_print();
         }
-        if (right != nullptr)
+        if (m_right != nullptr)
         {
-            right->postorder_print();
+            m_right->postorder_print();
         }
-        std::cout << value << std::endl;
+        std::cout << m_value << std::endl;
     }
 
     void inorder_print()
     {
-        if (left != nullptr)
+        if (m_left != nullptr)
         {
-            left->inorder_print();
+            m_left->inorder_print();
         }
-        std::cout << value << std::endl;
-        if (right != nullptr)
+        std::cout << m_value << std::endl;
+        if (m_right != nullptr)
         {
-            right->inorder_print();
+            m_right->inorder_print();
+        }
+    }
+
+    int left_child_height()
+    {
+        int height = -1;
+        if (m_left != nullptr)
+        {
+            height = m_left->m_height;
+        }
+
+        return height;
+    }
+
+    int right_child_height()
+    {
+        int height = -1;
+        if (m_right != nullptr)
+        {
+            height = m_right->m_height;
+        }
+
+        return height;
+    }
+
+    void update_height()
+    {
+        m_height = std::max(left_child_height(), right_child_height()) + 1;
+    }
+
+    const Comparable &find_max() const
+    {
+        if (m_right == nullptr)
+        {
+            return m_value;
+        }
+        else
+        {
+            return m_right->find_max();
+        }
+    }
+
+    const Comparable &find_min() const
+    {
+        if (m_left == nullptr)
+        {
+            return m_value;
+        }
+        else
+        {
+            return m_left->find_min();
+        }
+    }
+
+    /**
+     * @brief return an indication of whether or not the tree rooted
+     * at this node contains the specified element
+     *
+     * @param x the element to determine whether or not is it is in the tree
+     * rooted at this node
+     *
+     * @returns true if x is found in the tree, false otherwise
+     */
+    bool contains(const Comparable &x) const
+    {
+        if (x < m_value)
+        {
+            if (m_left != nullptr)
+            {
+                return m_left->contains(x);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (x > m_value)
+        {
+            if (m_right != nullptr)
+            {
+                return m_right->contains(x);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    /**
+     * @brief print the elements of the subtree rooted at the current node by
+     * depth
+     *
+     * print out all the nodes at depth print_depth; followed by all the nodes
+     * at print_depth + 1; followed by all the nodes at print_depth + 2; etc.
+     *
+     * @param current_depth the depth of the tree we are currently "at"
+     * @param print_depth the depth of the nodes that should be printed
+     */
+    void print_depth(int current_depth, int print_depth)
+    {
+        if (current_depth > print_depth)
+        {
+            // if we are deeper in the tree than the print_depth,
+            // then we are done.
+            return;
+        }
+        else if (current_depth == print_depth)
+        {
+            // if we are at the print_depth, then print the node value
+            std::cout << "depth: " << current_depth << " value: " << m_value
+                      << " height: " << m_height << std::endl;
+        }
+        else
+        {
+            // if we are not yet deep enough in the tree to print,
+            // go one level deeper wherever we can
+            if (m_left != nullptr)
+            {
+                m_left->print_depth(current_depth + 1, print_depth);
+            }
+            if (m_right != nullptr)
+            {
+                m_right->print_depth(current_depth + 1, print_depth);
+            }
         }
     }
 };
+
+/**
+ * @brief determine the height of the given BinarySearchTreeNode.
+ *
+ * @returns the height of the given BinarySearchTreeNode
+ */
+template <typename Comparable>
+int height(BinarySearchTreeNode<Comparable> *node)
+{
+    if (node == nullptr)
+    {
+        return -1;
+    }
+    else
+    {
+        return node->m_height;
+    }
+}
+
+/**
+ * @brief determine the balance factor for the specified BinarySearchTreeNode
+ *
+ * @returns positive value means left child is taller than the right child;
+ * negative value means right child is taller than left child; magnitude of
+ * value is how much one child is taller than the other; 0 value means
+ * children have the same height
+ */
+template <typename Comparable>
+int calculate_balance(BinarySearchTreeNode<Comparable> *node)
+{
+    if (node == nullptr)
+    {
+        return 0;
+    }
+    else
+    {
+        return height(node->m_left) - height(node->m_right);
+    }
+}
+
+/**
+ * @brief insert a node with the specified value (x) in the subtree rooted at the
+ * specified node (node)
+ *
+ * @param node the node which is the root of the subtree into which to insert a
+ * new node
+ * @param x the value to be inserted
+ *
+ * @returns the root of the newly inserted subtree.
+ */
+template <typename Comparable>
+BinarySearchTreeNode<Comparable> *insert_node(
+    BinarySearchTreeNode<Comparable> *node, const Comparable &value_to_insert)
+{
+    if (node == nullptr)
+    {
+        return new BinarySearchTreeNode<Comparable>(value_to_insert, nullptr, nullptr);
+    }
+    if (value_to_insert < node->m_value)
+    {
+        // insert into the left subtree
+        node->m_left = insert_node(node->m_left, value_to_insert);
+    }
+    else if (node->m_value < value_to_insert)
+    {
+        // insert into the right subtree
+        node->m_right = insert_node(node->m_right, value_to_insert);
+    }
+    else
+    {
+        // x must be equal to the current node's value
+        // no need to create a new node, insert it, or balance anything
+        return node;
+    }
+    node->update_height();
+    int balance = calculate_balance(node);
+    return node;
+}
