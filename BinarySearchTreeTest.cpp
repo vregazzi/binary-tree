0746fd78e8c33f8be8e771ee1b42da2e67cb1bf9
Vincent Regazzi <vincentregazzi@gmail.com>
2023-11-27 19:46:48 -0600

    add simple insertion tests

--- BinarySearchTreeTest.cpp
+++ BinarySearchTreeTest.cpp
@@ -2,9 +2,10 @@ using namespace std;
 
 #include "BinarySearchTree.h"
 
-int main()
+int simple_insertion()
 {
     BinarySearchTree<int> tree;
+    cout << "Simple insertion testing" << endl;
     tree.insert(17);
     tree.insert(25);
     tree.insert(4);
@@ -12,6 +13,30 @@ int main()
     tree.insert(1);
     tree.insert(6);
     tree.insert(5);
+
+    cout << "contains 17: " << endl;
+    cout << tree.contains(17) << endl; // should be 1
+    cout << endl;
+
+    cout << "contains 25: " << endl;
+    cout << tree.contains(25) << endl; // should be 1
+    cout << endl;
+
+    cout << "contains 90: " << endl;
+    cout << tree.contains(90) << endl; // should be 0
+    cout << endl;
+
+    cout << "contains 0: " << endl;
+    cout << tree.contains(0) << endl; // should be 0
+    cout << endl;
+
+    cout << "find_min: " << endl;
+    cout << tree.find_min() << endl; // should be 1
+    cout << endl;
+
+    cout << "find_max: " << endl;
+    cout << tree.find_max() << endl; // should be 88
+    cout << endl;
 
     cout << "inorder_print" << endl;
     tree.inorder_print();
@@ -23,6 +48,16 @@ int main()
 
     cout << "postorder_print" << endl;
     tree.postorder_print();
+    cout << endl;
+
+    cout << "depthorder_print" << endl;
+    tree.depthorder_print();
+    cout << endl;
 
     return 0;
+}
+
+int main()
+{
+    simple_insertion();
 }
